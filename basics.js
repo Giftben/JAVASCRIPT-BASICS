777f858a320427c4731b31a1a316fe8ffd1dd6b8
Giftben <benjamingift2000@yahoo.com>
2022-10-14 19:37:47 +0100

    learning loops

---
+++ basics.js
@@ -0,0 +1,27 @@
+// var johnBmi, markBmi,johnHeight, markHeight, johnMass, markMass ;
+// markBmi = 12;
+// console.log(markBmi);
+
+// const array =[2,5,6,7,8,9,10,50]
+// let result =[]
+// let i = 0
+//  while (i < array.length){
+//      result.push(array[i] * 3)
+//  i++
+//     }
+//  console.log(result);
+
+//  do{
+//     result.push(array[i] * 3)
+//     i++
+//  }
+//  while(i < array.length)
+//  console.log(result)
+
+
+//  // write a code which prints 1-100
+//  let number = []
+//  for(let i = 1; i < 101; i++){
+//     number.push(i)
+//  }
+//  console.log(number)
